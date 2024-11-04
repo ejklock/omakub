c8f51ddafbc13f858c08e751b7da0987badae6ca
Evaldo Klock <neto.nemesis@gmail.com>
2024-11-04 13:50:00 -0400

    support pop os

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -1,3 +1,24 @@
-sudo apt install -y flatpak
-sudo apt install -y gnome-software-plugin-flatpak
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#!/bin/bash
+
+# Function to check if running on Ubuntu 24.04 or higher
+check_ubuntu_version() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [ "$ID" = "pop" ]; then
+      echo "$ID"
+    else
+      echo "Error: This script must be run on Ubuntu. Current OS: $ID" >&2
+      return 1
+    fi
+  else
+    echo "Error: Unable to determine OS. /etc/os-release file not found." >&2
+    return 1
+  fi
+}
+
+if ! check_ubuntu_version; then
+  sudo apt install -y flatpak
+  sudo apt install -y gnome-software-plugin-flatpak
+  sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+fi
+
