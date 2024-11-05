488083b62e9ae3b0da9601058b7ff946e7df65d9
Evaldo Klock <neto.nemesis@gmail.com>
2024-11-05 09:57:25 -0400

    fix

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,2 +1,8 @@
 # Add makedeb repository and install eza
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+
 sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
